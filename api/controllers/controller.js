244aea7f4a983293c2961f1a6919a52f4bcae4c8
GPlisga <gaston_plisga@hotmail.com>
2019-09-29 14:48:45 -0300

    Add function setPost(req, res) on controller.js

--- api/controllers/controller.js
+++ api/controllers/controller.js
@@ -5,10 +5,19 @@ var dbServices = require('../services/dbservices');
 
 // Respond with the posts stored in the database.
 function getAllPosts(req, res) {
-  res.send(dbServices.getAllPosts());
+  dbServices.getAllPosts().then(posts => {
+    res.send(posts);
+  }).catch(error => {
+    res.status(500).send(error);
+  });
+}
+
+function setPost(req, res) {
+  res.send(dbServices.setPost(req.body));
 }
 
 // Export the functions.
 module.exports = {
-  getAllPosts
+  getAllPosts,
+  setPost
 }
