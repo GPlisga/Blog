5e5df7ded9ac57e2c9c6ed4ade548e602f232bcb
GPlisga <gaston_plisga@hotmail.com>
2019-09-29 14:45:31 -0300

    Add function setPost() on dbservices.js

--- api/services/dbservices.js
+++ api/services/dbservices.js
@@ -4,32 +4,18 @@
 const db = require('../database/db');
 const Posts = db.models.posts;
 
-// Used to simulate a database temporarily.
-/*const posts = [
-  {
-    id: 0,
-    title: "Title post 1",
-    description: "Description post 1",
-  },
-  {
-    id: 1,
-    title: "Title post 2",
-    description: "Description post 2",
-  },
-  {
-    id: 2,
-    title: "Title post 3",
-    description: "Description post 3",
-  }
-];*/
-
 // Returns all posts stored in the database.
 function getAllPosts() {
-  //return posts;
   return Posts.findAll({});
+}
+
+// Create new post.
+function setPost(body) {
+  return Posts.create({title: body.title, description: body.description});
 }
 
 // Export the functions.
 module.exports = {
-  getAllPosts
+  getAllPosts,
+  setPost
 }
